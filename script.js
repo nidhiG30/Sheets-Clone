8bbb3d265358a1e2b9ff93831b4e0fb045bcc872
nidhiG30 <gadgenidhi9@gmail.com>
2023-05-31 21:12:51 +0530

    Created and styled the cells. Adjusted row and column number visibility.

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let cols = 26; // total alphabets are 26
 
 let addressRowCont = document.querySelector('.address-row-cont');
 let addressColCont = document.querySelector('.address-col-cont');
+let cellsCont = document.querySelector('.cells-cont');
 
 for (let i = 0; i < rows; i++) {
   let addressRow = document.createElement('div');
@@ -16,4 +17,16 @@ for (let i = 0; i < cols; i++) {
   addressCol.setAttribute('class', 'address-col');
   addressCol.innerText = String.fromCharCode(65 + i);
   addressColCont.appendChild(addressCol);
+}
+
+for (let i = 0; i < rows; i++) {
+  let rowCont = document.createElement('div');
+  rowCont.setAttribute('class', 'row-cont');
+  for (let j = 0; j < cols; j++) {
+    let cell = document.createElement('div');
+    cell.setAttribute('class', 'cell');
+    cell.setAttribute('contentEditable', 'true');
+    rowCont.appendChild(cell);
+  }
+  cellsCont.appendChild(rowCont);
 }
