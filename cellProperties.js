d5fd6798a9d34b2e7af40f7f4e724448b16be8d4
nidhiG30 <gadgenidhi9@gmail.com>
2023-06-01 16:20:39 +0530

    Data in the cells can be accessed & modified, but need to be put inside a storage. This storage is an array made like a matrix of 2D. The 2nd dimention of the storage array (the DB) consists of all the properties of an individual cell, which is structured inside an object.

--- cellProperties.js
+++ cellProperties.js
@@ -1,2 +1,20 @@
 // Storage
 let sheetDB = [];
+
+for (let i = 0; i < rows; i++) { // rows & cols are fetched from 'grid.js', bcz it is loaded before this file in the DOM
+  let sheetRow = []; // containes 1-100 cell's collected data
+  for (let j = 0; j < cols; j++) {
+    let cellProp = {
+      bold: false,
+      italic: false,
+      underline: false,
+      alignment: 'screenLeft',
+      fontFamily: 'monospace',
+      fontSIze: 14,
+      fontColor: '#000000',
+      BGcolor: '#000000', // default color
+    };
+    sheetRow.push(cellProp);
+  }
+  sheetDB.push(sheetRow);
+}
