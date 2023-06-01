e54979bef113c9fd99aca3b02c6d84fa328ea5db
nidhiG30 <gadgenidhi9@gmail.com>
2023-06-01 18:25:56 +0530

    Applied two way binding on the cells, such that upon data modification in the cells, the state will get changed and should retain that state in some storage. This way UI and storage are in sync.

--- cellProperties.js
+++ cellProperties.js
@@ -31,3 +31,55 @@ let alignment = document.querySelectorAll('.alignment');
 let leftAlign = alignment[0];
 let centerAlign = alignment[1];
 let rightAlign = alignment[2];
+let activeColorProp = '#d1d8e0';
+let inactiveColorProp = "#ecf0f1";
+
+// Application of two-way binding
+// Attach property listeners
+bold.addEventListener('click', e => {
+  let address = addressBar.value;
+  let [cell, cellProp] = activeCell(address);
+
+  // Modifictaion
+  cellProp.bold = !cellProp.bold; // Data Change
+  cell.style.fontWeight = cellProp.bold ? 'bold' : 'normal'; // UI change (1)
+  bold.style.backgroundColor = cellProp.bold ? activeColorProp : inactiveColorProp; // UI change (2)
+})
+
+italic.addEventListener('click', e => {
+  let address = addressBar.value;
+  let [cell, cellProp] = activeCell(address);
+
+  // Modifictaion
+  cellProp.italic = !cellProp.italic; // Data Change
+  cell.style.fontStyle = cellProp.italic ? 'italic' : 'normal'; // UI change (1)
+  italic.style.backgroundColor = cellProp.italic ? activeColorProp : inactiveColorProp; // UI change (2)
+})
+
+underline.addEventListener('click', e => {
+  let address = addressBar.value;
+  let [cell, cellProp] = activeCell(address);
+
+  // Modifictaion
+  cellProp.underline = !cellProp.underline; // Data Change
+  cell.style.textDecoration = cellProp.underline ? 'underline' : 'none'; // UI change (1)
+  underline.style.backgroundColor = cellProp.underline ? activeColorProp : inactiveColorProp; // UI change (2)
+})
+
+function activeCell(address) {
+  let [rid, cid] = decodeRidCidFromAddress(address);
+
+  // Access cell & storgae object
+  let cell = document.querySelector(`.cell[rid="${rid}"][cid="${cid}"]`);
+  let cellProp = sheetDB[rid][cid];
+  return [cell, cellProp]; // UI change: cell; data change: cellProp <- achieve two-way binding
+}
+
+// to decode the value of row id & col id
+function decodeRidCidFromAddress(address) {
+  // address => "A1": A = col; 1 = row
+  let rid = Number(address.slice(1) - 1); // -1' bcz the row indexing starts from 0
+  let cid = Number(address.charCodeAt(0)) - 65; // -65' to decode the numeric value
+
+  return [rid, cid];
+}
