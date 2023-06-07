7673a25a0467856602e3b34df0b778d9e39ff194
nidhiG30 <gadgenidhi9@gmail.com>
2023-06-07 19:36:03 +0530

    Created a storage, which will keep account of the total rows and columns in those rows in a 2D array form.

---
+++ cycleValidation.js
@@ -0,0 +1,11 @@
+// Storage -> 2D matrix (Basic needed)
+let graphComponentMatrix = [];
+
+for (let i = 0; i < rows.length; i++) {
+  let row = [];
+  for (let j = 0; j < cols.length; j++) {
+    // Why array -> More than 1 child relation (dependencies)
+    row.push([]);
+  }
+  graphComponentMatrix.push(row);
+}
