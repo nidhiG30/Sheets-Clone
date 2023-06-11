d527a6b659e956da14d5363fd9d3c387f1888dcb
nidhiG30 <gadgenidhi9@gmail.com>
2023-06-11 11:31:20 +0530

    Created default sheet.

--- sheetsHandling.js
+++ sheetsHandling.js
@@ -2,7 +2,9 @@ let addSheetBtn = document.querySelector('.sheet-add-icon');
 addSheetBtn.addEventListener('click', () => {
   let sheet = document.createElement('div');
   sheet.setAttribute('class', 'sheet-folder');
-  
+
   let allSheetFolders = document.querySelectorAll('.sheet-folder');
   sheet.setAttribute('id', allSheetFolders.length);
-})
+
+  sheet.innerHTML = `<div class="sheet-content">Sheet ${allSheetFolders.length + 1}</div>`;
+});
