8791f3cb9ba8218f682e96b8e8f5787e2c809b1c
nidhiG30 <gadgenidhi9@gmail.com>
2023-06-11 11:35:13 +0530

    Add sheets in the sheets folder, so on click of sheet-add-icon, the sheets will get added.

--- sheetsHandling.js
+++ sheetsHandling.js
@@ -1,3 +1,4 @@
+let sheetsFolderContainer = document.querySelector('.sheets-folder-cont');
 let addSheetBtn = document.querySelector('.sheet-add-icon');
 addSheetBtn.addEventListener('click', () => {
   let sheet = document.createElement('div');
@@ -7,4 +8,6 @@ addSheetBtn.addEventListener('click', () => {
   sheet.setAttribute('id', allSheetFolders.length);
 
   sheet.innerHTML = `<div class="sheet-content">Sheet ${allSheetFolders.length + 1}</div>`;
+
+  sheetsFolderContainer.appendChild(sheet);
 });
