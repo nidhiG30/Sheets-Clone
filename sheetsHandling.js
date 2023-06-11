c684e87e1b9962260f5068898fc6761c5b2f76c5
nidhiG30 <gadgenidhi9@gmail.com>
2023-06-11 11:22:58 +0530

    Set ID, Class and created sheet. Default ID provided to sheet-folder is '0' because array indexing starts from '0'. And when 'allSheetFolder' is accessed which has default ID of '0', its length turn out to be '1', which is the minimum requirement to have at least one sheet in the app.

--- sheetsHandling.js
+++ sheetsHandling.js
@@ -2,4 +2,7 @@ let addSheetBtn = document.querySelector('.sheet-add-icon');
 addSheetBtn.addEventListener('click', () => {
   let sheet = document.createElement('div');
   sheet.setAttribute('class', 'sheet-folder');
+  
+  let allSheetFolders = document.querySelectorAll('.sheet-folder');
+  sheet.setAttribute('id', allSheetFolders.length);
 })
