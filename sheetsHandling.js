9ce8d4f9e30728a53f66c47e157623bed976b15c
nidhiG30 <gadgenidhi9@gmail.com>
2023-06-11 11:06:51 +0530

    Folder to Add Multiple Sheets Management with Synchronous Storage.

---
+++ sheetsHandling.js
@@ -0,0 +1,5 @@
+let addSheetBtn = document.querySelector('.sheet-add-icon');
+addSheetBtn.addEventListener('click', () => {
+  let sheet = document.createElement('div');
+  sheet.setAttribute('class', 'sheet-folder');
+})
