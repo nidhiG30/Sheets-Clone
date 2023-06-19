9565203b1d1fedf0b2065c734e4f38dc53889d2a
nidhiG30 <gadgenidhi9@gmail.com>
2023-06-19 15:07:18 +0530

    To upload a sheet, we need some data that is read by JSON.parse to encode it. This encoded data is further pushed inside the collected sheetDB and graphComponent.

--- openSave.js
+++ openSave.js
@@ -12,3 +12,24 @@ downloadBtn.addEventListener('click', e => {
   a.click();
 });
 
+// Open Task (Upload)
+openBtn.addEventListener('click', e => {
+  // Opens file explorer
+  let input = document.createElement('input');
+  input.setAttribute('type', 'file');
+  input.click();
+
+  input.addEventListener('change', e => {
+    let fr = new FileReader();
+    let files = input.files; // Returns a FileList object on a file type input object.
+    let fileObj = files[0]; // Taking only 1 file, which is present in files[0]
+
+    fr.readAsText(fileObj);
+    fr.addEventListener('load', e => {
+      let readSheetData = JSON.parse(fr.result);
+
+      // Basic sheet with default data will be created
+      addSheetBtn.click();
+    })
+  });
+})
